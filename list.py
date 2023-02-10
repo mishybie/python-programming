c6647fbb70975ecf8865c9815b76607217c11f82
unknown <cwmitchelle@gmail.com>
2023-02-10 10:51:26 -0800

    python lists

---
+++ list.py
@@ -0,0 +1,96 @@
+
+
+numbers=[45,67,89,98,56,67,76]
+
+#names["python","liam","kwame"]
+# print(len(numbers))
+
+# print(sum(numbers))
+
+# for i in range(len(numbers)):
+#     print(numbers[i])
+
+    #LIST METHOD
+    #index
+    #print()
+
+# print(numbers.index(89))
+
+# print(#names.index["liam"])
+
+#append.adds an element to the end of a list
+# before appending
+# for x in range (len (numbers)):
+#     print(numbers[x])
+
+# numbers.append (89)
+
+# #fter appending
+# for i in range (len(numbers)):
+#     print(numbers[i])
+
+# #count-returns the number of times an element occurs in a list
+
+# print(numbers.count(67))
+
+# #pop-removes an element at a given index and returns that element
+# print(numbers.pop(3))
+
+# #insert -inserts an element x  at an index y
+# numbers.insert(2,100)
+
+# for x in range (len(numbers)):
+#     print(numbers[x])
+
+# #remove-removes the first occurence of an element 
+
+# numbers.remove (67)
+
+# #sort-arranges the list in ascendig order
+# for i in range (len(numbers)):
+#     print(numbers[x])
+#     #sort the list 
+# numbers.sort()
+
+# add a new line of formmated output
+
+# print("\n")
+
+# for i in range (len(numbers)):
+#     print(numbers[x])
+
+
+#TIPS AND TRICKS
+#multiplying a list
+# 
+# zeros=[0*10]
+
+# #adding two list 
+# list1=(56,89)
+# list2=(34,78)
+
+# #slicing a list 
+# nums=(12,45,78,67)
+
+# #get the maximum value in a list 
+# max(nums)
+
+# #get the minimum value in a list
+# min(nums)
+# #creating a list
+# nums =[]#emptyy list
+
+# for i in range(10):
+#     #insert elements in a list
+#     nums.append(i)
+
+# for i in range (len(nums)):
+#     print(nums[i])  
+
+even_nums=[]
+for num in range(20):
+    if (num%2==0):
+        even_nums.append(num)
+        
+for i in range(len(even_nums)):
+    print(even_nums[i])
