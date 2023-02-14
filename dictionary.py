7549d2c9803d05a7c3fe5c8609588ed3dd212805
unknown <cwmitchelle@gmail.com>
2023-02-14 00:10:52 -0800

    python

---
+++ dictionary.py
@@ -0,0 +1,52 @@
+# cars[
+#     "model":"ford",
+#     "year":1962
+# ]
+
+# print(cars)["model"]
+
+
+# vehicles={"name1":{"type":"ford"},
+#          "name2":{"type":"mercidez"},
+# }
+
+# print(vehicles["name2"]["type"])
+
+
+
+# def my_function(first_name):
+#     print(first_name+ "hello")
+# my_function("cynthia")
+# my_function("linus")
+
+# def input(second_name):
+#     print(second_name+ "hello")
+# input("wambui")
+# input("wanjiru")
+
+
+# Name=input("Enter your name")
+
+# def my_function(first_name):
+#     print(first_name+ "hello")
+# my_function("mishy") 
+# my_function("ivy")
+
+
+
+# a=22
+# b=47
+
+# addition=("a+b")
+# multiplication=("a*b")
+# division=("a/2")
+# remainder=("b%")
+
+# print(addition=("a+b"))
+# print(multiplication=("a*b"))
+# print(division=("a/2"))
+# print(remainder=("b%"))
+
+
+numbers=[9,7,9,5]
+x=num[0]
---
+++ strings.py
@@ -0,0 +1,60 @@
+name="climate"
+
+#index
+# print(name[0])
+
+# print(len(name))
+
+# print(name[0])
+
+# for letter in name:
+#     print(letter)
+# print("-"*5)
+
+# name1="edwin"
+# name2="gachiri"
+# #
+# print(name1+name2)
+
+
+# name=input("enter name:")
+
+# print("hello"+name)
+
+#special characters
+# name="edwin"
+# name1="gachiri"
+# print(name)
+# print("\n")
+# print(name1)
+
+
+#string methods
+
+#uppermethod-converts lower case to upper case
+# person="mitchelle"
+# print(person.upper())
+# print(person.lower())
+
+#checking for alphanumeric
+
+
+
+#ask for a user name
+
+#username=edwin
+#print"Hello username"
+#Hello EDWIN
+#use a function
+
+# def greet_user():
+#     username=input("enter your username:")
+#     print("hello"+username.upper()+"!")
+
+#     greet_user()
+
+# type="x123edwin456"
+# print(type[4:6])
+
+email="bob123@gmail.com"
+print(email[::-1])
